2d549aae83f67165815a79518f80567be4388bdd
KimHeeJin <heejin@utransfer.com>
2022-09-18 00:22:14 +0900

    [V1] service, repository에도 trace 적용

--- src/main/java/hello/advanced/app/v1/OrderRepositoryV1.java
+++ src/main/java/hello/advanced/app/v1/OrderRepositoryV1.java
@@ -1,5 +1,7 @@
 package hello.advanced.app.v1;
 
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -7,11 +9,21 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class OrderRepositoryV1 {
 
+    private final HelloTraceV1 trace;
+
     public void save(String itemId) {
-        if("ex".equals(itemId)) {
-            throw new IllegalStateException("예외 발생!!");
+        TraceStatus status = null;
+        try{
+            status = trace.begin("OrderRepositoryV1.save()");
+            if("ex".equals(itemId)) {
+                throw new IllegalStateException("예외 발생!!");
+            }
+            this.sleep(1000);
+            trace.end(status);
+        } catch(Exception e) {
+            trace.exception(status, e);
+            throw e;
         }
-        this.sleep(1000);
     }
 
     private void sleep(int millis) {
