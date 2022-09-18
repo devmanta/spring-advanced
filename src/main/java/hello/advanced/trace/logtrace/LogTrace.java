7fe1add930cf51505bf3d5c790b84417337805d7
KimHeeJin <heejin@utransfer.com>
2022-09-18 23:51:28 +0900

    traceId를 파라미터 넘기는 방식이 아닌 클이슈 래스 인스턴스 변수에 보관해서 사용해보기 (but 동기화 이슈 있음)

---
+++ src/main/java/hello/advanced/trace/logtrace/LogTrace.java
@@ -0,0 +1,11 @@
+package hello.advanced.trace.logtrace;
+
+import hello.advanced.trace.TraceStatus;
+
+public interface LogTrace {
+
+    TraceStatus begin(String msg);
+    void end(TraceStatus status);
+    void exception(TraceStatus status, Exception e);
+
+}
