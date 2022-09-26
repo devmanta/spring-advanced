f74bef1786a10c150e7f4d7ad3ee2f0797532773
KimHeeJin <heejin@utransfer.com>
2022-09-27 01:28:34 +0900

    전략패턴 - 파라미터로 전달 받기

---
+++ src/test/java/hello/advanced/trace/strategy/code/strategy/ContextV2.java
@@ -0,0 +1,17 @@
+package hello.advanced.trace.strategy.code.strategy;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 필드에 전략을 보관하는 방식
+ */
+@Slf4j
+public class ContextV2 {
+
+    public void execute(Strategy strategy) {
+        long start = System.currentTimeMillis();
+        strategy.call(); //위임!!
+        long end = System.currentTimeMillis();
+        log.info("실행시간={}", (end - start));
+    }
+}
