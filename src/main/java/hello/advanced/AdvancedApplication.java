df92ad684b6c1a6a9375cdde9de555dc758e7ef8
KimHeeJin <heejin@utransfer.com>
2022-10-03 19:37:30 +0900

    start시 import, scanBasePackages 명시 설명 주석

--- src/main/java/hello/advanced/AdvancedApplication.java
+++ src/main/java/hello/advanced/AdvancedApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-//@Import(AppV1Config.class)
-//@SpringBootApplication(scanBasePackages = "hello.advanced.part2.proxy.app")
+//@Import(AppV1Config.class) //나중에 v2, v3 로 바꿀 때, v2, v3만 적용하고 싶어서
+//@SpringBootApplication(scanBasePackages = "hello.advanced.part2.proxy.app") //이렇게 명시하면 이 하위 패키지만 componentScan대상이 된다.
 public class AdvancedApplication {
 
 	public static void main(String[] args) {
