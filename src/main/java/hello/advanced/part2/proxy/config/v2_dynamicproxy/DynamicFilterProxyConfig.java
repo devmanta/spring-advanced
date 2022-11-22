a47f759ae589b2ab1a3f148d37a0de4baf88594c
KimHeeJin <dlwlrgkwk82@gmail.com>
2022-11-23 01:42:11 +0900

    add JDK dynamic proxy filter example (only long for specific patterns)

---
+++ src/main/java/hello/advanced/part2/proxy/config/v2_dynamicproxy/DynamicFilterProxyConfig.java
@@ -0,0 +1,54 @@
+package hello.advanced.part2.proxy.config.v2_dynamicproxy;
+
+import hello.advanced.part1.trace.logtrace.LogTrace;
+import hello.advanced.part2.proxy.app.v1.ProxyOrderControllerV1;
+import hello.advanced.part2.proxy.app.v1.ProxyOrderControllerV1Impl;
+import hello.advanced.part2.proxy.app.v1.ProxyOrderRepositoryV1;
+import hello.advanced.part2.proxy.app.v1.ProxyOrderRepositoryV1Impl;
+import hello.advanced.part2.proxy.app.v1.ProxyOrderServiceV1;
+import hello.advanced.part2.proxy.app.v1.ProxyOrderServiceV1Impl;
+import hello.advanced.part2.proxy.config.v2_dynamicproxy.handler.LogTraceBasicHandler;
+import hello.advanced.part2.proxy.config.v2_dynamicproxy.handler.LogTraceFilterHandler;
+import java.lang.reflect.Proxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DynamicFilterProxyConfig {
+
+    private static final String[] PATTERNS = {"request*", "order*", "save*"};
+
+    @Bean
+    public ProxyOrderControllerV1 proxyOrderControllerV1(LogTrace logTrace) {
+        ProxyOrderControllerV1 proxyOrderControllerV1 = new ProxyOrderControllerV1Impl(proxyOrderServiceV1(logTrace));
+
+        ProxyOrderControllerV1 proxy = (ProxyOrderControllerV1) Proxy.newProxyInstance(proxyOrderControllerV1.getClass().getClassLoader(),
+            new Class[]{ProxyOrderControllerV1.class},
+            new LogTraceFilterHandler(proxyOrderControllerV1, logTrace, PATTERNS)); // LogTraceBasicHandler 이거는 빈등록 안하고 각각 인스턴스를 만들었다. 왜냐면 각각 target이 다르기 때문에 LogTraceBasicHandler에 있는 전역변수가 달라져서
+
+        return proxy;
+    }
+
+    @Bean
+    public ProxyOrderServiceV1 proxyOrderServiceV1(LogTrace logTrace) {
+        ProxyOrderServiceV1 proxyOrderServiceV1 = new ProxyOrderServiceV1Impl(proxyOrderRepositoryV1(logTrace));
+
+        ProxyOrderServiceV1 proxy = (ProxyOrderServiceV1) Proxy.newProxyInstance(proxyOrderServiceV1.getClass().getClassLoader(),
+            new Class[]{ProxyOrderServiceV1.class},
+            new LogTraceFilterHandler(proxyOrderServiceV1, logTrace, PATTERNS));
+
+        return proxy;
+    }
+
+    @Bean
+    public ProxyOrderRepositoryV1 proxyOrderRepositoryV1(LogTrace logTrace) {
+        ProxyOrderRepositoryV1 proxyOrderRepositoryV1 = new ProxyOrderRepositoryV1Impl();
+
+        ProxyOrderRepositoryV1 proxy = (ProxyOrderRepositoryV1) Proxy.newProxyInstance(proxyOrderRepositoryV1.getClass().getClassLoader(),
+            new Class[]{ProxyOrderRepositoryV1.class},
+            new LogTraceFilterHandler(proxyOrderRepositoryV1, logTrace, PATTERNS));
+
+        return proxy;
+    }
+
+}
---
+++ src/main/java/hello/advanced/part2/proxy/config/v2_dynamicproxy/handler/LogTraceFilterHandler.java
@@ -0,0 +1,44 @@
+package hello.advanced.part2.proxy.config.v2_dynamicproxy.handler;
+
+import hello.advanced.part1.trace.TraceStatus;
+import hello.advanced.part1.trace.logtrace.LogTrace;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.PatternMatchUtils;
+
+@Slf4j
+public class LogTraceFilterHandler implements InvocationHandler {
+
+    private final Object target;
+    private final LogTrace logTrace;
+    private final String[] patterns;
+
+    public LogTraceFilterHandler(Object target, LogTrace logTrace, String[] patterns) {
+        this.target = target;
+        this.logTrace = logTrace;
+        this.patterns = patterns;
+    }
+
+    @Override
+    public Object invoke(Object o, Method method, Object[] args) throws Throwable {
+
+        String methodName = method.getName();
+        if(!PatternMatchUtils.simpleMatch(patterns, methodName)) {
+            return method.invoke(target, args);
+        }
+
+        TraceStatus status = null;
+        try{
+            String message = method.getDeclaringClass().getSimpleName() + "." + method.getName();
+            status = logTrace.begin(message);
+            Object result = method.invoke(target, args);
+            logTrace.end(status);
+            return result;
+        } catch(Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+
+}
